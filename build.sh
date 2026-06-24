7ad3719c5b104b3a2349041f9f20f2b79176dc8b
fnnk <fnnktkygl@gmail.com>
2026-06-24 11:57:23 -0400

    Optimize build.sh to prevent Vercel Out Of Memory crashes

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # Limite la mémoire de la VM Dart pour éviter l'erreur "Out of Memory" (exit code -9) sur Vercel
-export DART_VM_OPTIONS="--old_gen_heap_size=800"
+export DART_VM_OPTIONS="--old_gen_heap_size=512"
 
 if cd flutter; then
   git pull
@@ -11,4 +11,5 @@ fi
 ./flutter/bin/flutter config --no-analytics
 ./flutter/bin/flutter pub get
 
-./flutter/bin/flutter build web --release --no-wasm-dry-run
+# Add --no-tree-shake-icons to avoid memory spikes during font tree-shaking
+./flutter/bin/flutter build web --release --no-wasm-dry-run --no-tree-shake-icons
