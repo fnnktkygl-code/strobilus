0336c6945cc453b10a6f5898079f21b23043b4e0
fnnk <fnnktkygl@gmail.com>
2026-06-24 09:13:11 -0400

    Fix Vercel OOM: Limit Dart VM memory

--- build.sh
+++ build.sh
@@ -7,4 +7,7 @@ else
 fi
 ./flutter/bin/flutter config --no-analytics
 ./flutter/bin/flutter pub get
+# Limite la mémoire de la VM Dart pour éviter l'erreur "Out of Memory" (exit code -9) sur Vercel
+export DART_VM_OPTIONS="--old_gen_heap_size=800"
+
 ./flutter/bin/flutter build web --release --no-wasm-dry-run
